3f7ce13a51d38a7b9b01bce069ad7d96c44fd99c
Naresh Kumar H <nareshkumarh@live.com>
2017-09-25 17:50:51 +0530

    Initial commit

---
+++ src/main/java/com/naresh/restapiapp/RestapiAppApplication.java
@@ -0,0 +1,12 @@
+package com.naresh.restapiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/naresh/restapiapp/RestapiAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.naresh.restapiapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestapiAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
